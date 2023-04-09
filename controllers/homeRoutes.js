58b587e7655dc417132f647d408ff3839427add9
jrob02180 <jrob02180@gmail.com>
2023-04-08 20:57:40 -0600

    added jass.css, added package-lock.json to gitignore

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,7 +13,7 @@ router.get('/', async (req, res) => {
             ],
         });
 
-        const blogs = blogData.map(blog) => blog.get({ plain: true });
+        const blogs = blogData.map((blog) => blog.get({ plain: true }));
 
         res.render('homepage', {
             blogs,
