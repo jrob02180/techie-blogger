ccabfd7e3ff0a837a21c33d4057e8c2e721b5e6e
jrob02180 <jrob02180@gmail.com>
2023-04-08 20:13:51 -0600

    Added code to models > User.js

---
+++ models/user.js
@@ -0,0 +1,58 @@
+const { Model, Datatypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPW, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: Datatypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: Datatypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            },
+        },
+        password: {
+            type: Datatypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4],
+            },
+        },
+    },
+    {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            beforeUpdate: async (updatedUserData) => {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+);
+
+module.exports = User;
