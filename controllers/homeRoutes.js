0adc93ba01c952d930e0057dc3790f4a7efdf60d
jrob02180 <jrob02180@gmail.com>
2023-04-14 21:23:51 -0600

    Added + New Post button to dashboard, commented out dashboard.js, redirected get to homepage on homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -15,8 +15,8 @@ router.get('/', async (req, res) => {
 
         const posts = postData.map((post) => post.get({ plain: true }));
 
-        res.render('dashboard', {
-            postss,
+        res.render('homepage', {
+            ...posts,
             logged_in: req.session.logged_in
         });
     } catch (err) {
